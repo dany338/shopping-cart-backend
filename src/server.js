d86f5e8b71cbe1968b7ebf7847a78ca2691d2e9d
Daniel Andres Gallo Garcia <dgallo@teravisiontech.com>
2020-07-26 17:27:16 -0500

    changes

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+var http = require('http');
 const express    = require('express');
 const serverless = require("serverless-http");
 const bodyParser = require('body-parser');
@@ -7,7 +8,7 @@ const products   = require('../api/routes/product.routes');
 const categories = require('../api/routes/category.routes');
 
 const app = express();
-
+var servidor = http.createServer(function(peticion, respuesta) {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -16,7 +17,9 @@ app.use('/api/orders', orders);
 app.use('/api/products', products);
 app.use('/api/categories', categories);
 const port = process.env.PORT || '5000';
-app.listen(port, () => console.log(`Servidor corriendo en localhost:${port}`));
-
+  // app.listen(port, () => console.log(`Servidor corriendo en localhost:${port}`));
+});
+servidor.listen(5000);
+console.log('Ejecutando un server local con un node.js');
 // module.exports = app;
 // module.exports.handler = serverless(app);
