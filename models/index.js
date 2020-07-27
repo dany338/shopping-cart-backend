d7b02be17da8110036487433bbc7c408f36837b8
Daniel Andres Gallo Garcia <dgallo@teravisiontech.com>
2020-07-26 20:53:12 -0500

    config driver mysql2

--- models/index.js
+++ models/index.js
@@ -3,16 +3,22 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+const mysql2 = require('mysql2');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'production';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
+const production = {
+  dialect: 'mysql',
+  dialectModule: mysql2, // Needed to fix sequelize issues with WebPack
+  host: '51.161.116.202',
+  port: 3306
+};
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, production);
 }
 
 fs
